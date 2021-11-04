8d02cebe07e77e7480815a36f5bb71d6e2dc323d
Reza Azerly <kiritosao080@gmail.com>
2021-11-04 19:39:59 +0700

    "Setup and initial commit"

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./app.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+            filename: "index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader"
+                ]
+            }
+        ]
+    }
+}
