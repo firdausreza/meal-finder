9016922d999a53c38b49ec17cfce8fdfc8f47bb6
Reza Azerly <kiritosao080@gmail.com>
2021-11-06 14:09:46 +0700

    rebase dist

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: "./app.js",
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "docs")
     },
     plugins: [
         new MiniCssExtractPlugin({
